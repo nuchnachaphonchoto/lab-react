efc0619de6d140b117ab890fcf1d542f9bbe297e
nuchnachaphonchoto <song290144@gmail.com>
2022-08-20 14:54:30 +0700

    commit 1 : add title and description

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import WordCard from './WordCard'
 
 function App() {
   return (
-    <div>
+    <div class="center">
+    <h1> Game Logic </h1>
     <WordCard value="hello"/>
+      
+      <p><b>Description : </b>Arrange the following letters in the correct words.</p>
+     
     </div>
     );
    
